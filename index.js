880eaada42d4f81786f0687bfa289c1cd3f1dce9
Lydia <lreitzel@gmail.com>
2021-08-01 17:55:48 -0400

    commit tree 08674226337763cf36a26f03ec62d1200660a87e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(array, name) {
+    return array.filter ( match =>
+        match.toLowerCase() === name.toLowerCase()
+    )
+}
+
+function fuzzyMatch(array, sample) {
+    return array.filter( maybeMatch =>
+        maybeMatch.toLowerCase().indexOf(sample.toLowerCase()) === 0
+    )
+}
+
+function matchName(array, name) {
+    return array.filter( record => record.name === name)
+}
